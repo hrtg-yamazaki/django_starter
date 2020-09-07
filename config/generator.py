1606254466d59f358ba3c029afefb23eda3b5600
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-09-07 16:15:50 +0900

    [update] config / generator
    ・自動生成されたシークレットキーを ./local_settings.py に書き込む関数を作成

--- config/generator.py
+++ config/generator.py
@@ -1,9 +1,17 @@
+import os
 import secrets
 import string
+from pathlib import Path
+
+
+os.chdir(Path(__file__).parent)
+LOCAL_SETTINGS_PATH = "./local_settings.py"
 
 
 def generate_secret_key():
-    """シークレットーキー自動生成"""
+    """
+    シークレットーキー自動生成
+    """
 
     sequence = string.ascii_letters + string.digits + "!#$%()+-@"
 
@@ -15,5 +23,29 @@ def generate_secret_key():
     return secret_key
 
 
-print(generate_secret_key())
+def secret_key_is_exist():
+    """
+    SECRET_KEY_LSの存在確認
+    """
+    with open(LOCAL_SETTINGS_PATH, "r", encoding="utf-8") as f:
+        for row in f:
+            if "SECRET_KEY_LS" in row:
+                print("SECRET_LEY_LS is already defined.")
+                return True
+        return False
 
+
+def write_on_ls():
+    """
+    generate_secret_keyで生成したシークレットキーを
+    local_settings.pyに書き込む
+    """
+    if not secret_key_is_exist():
+        with open(LOCAL_SETTINGS_PATH, "a", encoding="utf-8") as f:
+            secret_key = generate_secret_key()
+            f.write("SECRET_KEY_LS = \"" + secret_key + "\"\n")
+        print("SECRET_KEY_LS has been generated.")
+
+
+if __name__ == "__main__":
+    write_on_ls()
